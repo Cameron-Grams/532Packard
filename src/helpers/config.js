d3aa30732d96f1b150ad6c5f1b155011405181a2
Cameron <cameron.grams.2@gmail.com>
2018-04-13 09:19:42 -0400

    working heroku endpoint, move on to styling

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -2,4 +2,4 @@ export const apiKey = "AIzaSyA9SXLscqsL8V87xB16w2q52lO-0yzejDQ";
 
 // export const endpoint = "http://localhost:8080";
 
-export const endpoint = "https://still-refuge-96429.herokuapp.com/"
+export const endpoint = "https://still-refuge-96429.herokuapp.com"
