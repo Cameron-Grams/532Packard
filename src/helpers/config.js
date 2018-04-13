3e3d7a5e190b464b8cbcabdae889fc567d43663d
Cameron <cameron.grams.2@gmail.com>
2018-04-13 09:14:50 -0400

    new endpoint

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,5 @@
 export const apiKey = "AIzaSyA9SXLscqsL8V87xB16w2q52lO-0yzejDQ";
 
-export const endpoint = "http://localhost:8080";
+// export const endpoint = "http://localhost:8080";
 
+export const endpoint = "https://still-refuge-96429.herokuapp.com/"
