e6b10f5108e1a1a823ed2f40cee4634220234789
Cameron <cameron.grams.2@gmail.com>
2018-04-16 17:23:26 -0400

    polish

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -4,10 +4,15 @@ import Header from '../Header/Header';
 import MiddleSection from '../MiddleSection/MiddleSection'; 
 import ApartmentFooter from '../ApartmentFooter/ApartmentFooter'; 
 
+const spaceDiv = (
+    <div className="additionalSpaceDiv"></div>
+);
+
 const Main = ( props ) => (
     <div className={ "mainDiv" }>
         <Header mainPage={ true } />    
         <MiddleSection />
+        { spaceDiv }
         <ApartmentFooter /> 
     </div>
 );
